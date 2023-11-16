ba04225660806fe6e3cd8e9aa7099af153121a8f
Bryan Prather-Huff <bryan@pratherhuff.com>
2023-11-15 21:08:20 -0600

    direnv should be in user tool-versions, but echoed right

--- .devcontainer/asdf-post-create.sh
+++ .devcontainer/asdf-post-create.sh
@@ -4,7 +4,7 @@ cd "${0%/*}/.."
 
 asdf plugin-add direnv &&
 asdf direnv setup --shell zsh --version latest
-echo "direnv 2.32.3" /home/vscode/.tool-versions
+echo "direnv 2.32.3" >> /home/vscode/.tool-versions
 
 # find all .tool-versions within the repo, but ignore all hidden directories
 /bin/find /workspace -type d -path '*/.*' -prune -o -name '*.tool-version*' -print | while read filePath; do
