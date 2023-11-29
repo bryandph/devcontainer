ac554c2ef03fd81854c3837ab7b659f15f3c10ea
Bryan Prather-Huff <bryan@pratherhuff.com>
2023-11-28 22:27:45 -0600

    updating asdf-plugin-manager

--- .devcontainer/asdf-post-create.sh
+++ .devcontainer/asdf-post-create.sh
@@ -2,24 +2,29 @@
 
 cd "${0%/*}/.."
 
+asdf plugin add asdf-plugin-manager https://github.com/asdf-community/asdf-plugin-manager.git
+asdf install asdf-plugin-manager latest
+asdf global asdf-plugin-manager latest
 asdf plugin-add direnv
 asdf direnv setup --shell zsh --version latest
-echo "direnv 2.32.3" > /home/vscode/.tool-versions
+asdf global direnv latest
+
+asdf-plugin-manager export > ~/.plugin-versions
 
 
 # find all .tool-versions within the repo, but ignore all hidden directories
-/bin/find /workspaces -type d -path '*/.*' -prune -o -name '*.tool-version*' -print | while read filePath; do
+# /bin/find /workspaces -type d -path '*/.*' -prune -o -name '*.tool-version*' -print | while read filePath; do
+#   echo "asdf setup for $filePath"
+
+#   # install all required plugins
+#   cat $filePath | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf plugin add {}
+
+#   # install all required versions
+#   (cd $(dirname $filePath) && direnv allow && asdf install)
+# done
+
+/bin/find /workspaces -type d -path '*/.*' -prune -o -name '*.plugin-versions*' -print | while read filePath; do
   echo "asdf setup for $filePath"
-
-  # install all required plugins
-  cat $filePath | cut -d' ' -f1 | grep "^[^\#]" | xargs -i asdf plugin add {}
-
-  # install all required versions
-  (cd $(dirname $filePath) && direnv allow && asdf install)
+  cd $(dirname  "$filePath")
+  asdf-plugin-manager add-all
 done
-
-# automatically startup a docker-compose that exists in the devcontainer folder
-# if test -f $CODESPACE_VSCODE_FOLDER/.devcontainer/docker-compose.yml; then
-#   echo "docker-compose found, starting up"
-#   (cd $CODESPACE_VSCODE_FOLDER/.devcontainer && docker compose up -d)
-# fi
